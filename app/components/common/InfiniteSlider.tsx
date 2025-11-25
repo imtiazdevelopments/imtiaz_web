1123e3a20b2e72697cb460f23e7be2f44dbb5376
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-11-25 18:47:07 +0530

    fix(home components)

--- app/components/common/InfiniteSlider.tsx
+++ app/components/common/InfiniteSlider.tsx
@@ -3,20 +3,20 @@ import { motion } from "framer-motion";
 
 const MarqueeText = () => {
   return (
-    <div className="z-10 w-full h-fit bottom-0 overflow-hidden flex items-center absolute">
+    <div className="w-full h-fit bottom-0 overflow-hidden flex items-center absolute z-0">
       <motion.div
-        className="flex min-w-max space-x-10"
+        className="flex min-w-max space-x-8 opacity-50"
         animate={{ x: ["0%", "-100%"] }} // Moves seamlessly
         transition={{ repeat: Infinity, duration: 80, ease: "linear" }} // Increased duration for slower speed
       >
-        {[...Array(10)].map((_, i) => (
+        {[...Array(20)].map((_, i) => (
           /*  <motion.h1
           key={i}
             className="text-[300px] font-light text-transparent bg-clip-text bg-gradient-to-r from-gray-500 via-white to-gray-500 whitespace-nowrap leading-none"
 > */
           <motion.h1
             key={i}
-            className="text-[130px] font-light text-white font-[optima]"
+            className="text-[100px] 2xl:text-[130px] font-light text-white font-[optima]"
           >
             IMTIAZ .
           </motion.h1>
