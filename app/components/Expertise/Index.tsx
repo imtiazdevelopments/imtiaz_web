af84d11b905a68896359f618228f5892c9fa4c4f
jishnum007 <jishnu@globalsurf.ae>
2026-03-23 19:49:16 +0530

    expertise

---
+++ app/components/Expertise/Index.tsx
@@ -0,0 +1,15 @@
+import Hero from "./sections/Hero"; 
+import ThinkingThatDelivers from "./sections/thinking-that-delivers";
+import Faq from "./sections/Faqsection";
+import EnquirySection from "./sections/Enquiry";
+export default function Home() {
+  return (
+    <>
+    
+      <Hero />  
+      <ThinkingThatDelivers />
+      <Faq />
+    <EnquirySection />
+    </>
+  );
+}
