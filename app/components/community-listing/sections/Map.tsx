72d51511c59d7bac18e46d8e8d790f52c4de6694
Nived <nived@globalsurf.ae>
2026-04-10 12:18:51 +0530

    refactor:fake map to real map community-listing page

--- app/components/community-listing/sections/Map.tsx
+++ app/components/community-listing/sections/Map.tsx
@@ -1,9 +1,24 @@
+"use client";
 import Image from "next/image";
 
-const Map = () => {
+import { Map, Marker } from "@vis.gl/react-google-maps";
+import { APIProvider } from "@vis.gl/react-google-maps";
+
+interface Props {
+  latitude: string;
+  longitude: string;
+  title?: string;
+}
+
+const MapOriginal = () => {
+  const latitude = "25.1972";
+  const longitude = "55.2744";;
+  const lat = parseFloat(latitude);
+  const lng = parseFloat(longitude);
+
   return (
-    <section data-header="dark" className="w-full py-120 3xl:py-160">
-      <div className=" flex flex-col justify-center">
+    <section className="w-full h-[839px] py-120 3xl:py-160">
+      {/* <div className=" flex flex-col justify-center">
         <div>
           <Image
             src="/images/community-listing/map-2.jpg"
@@ -13,9 +28,26 @@ const Map = () => {
             className="w-full"
           />
         </div>
-      </div>
+      </div> */}
+      <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API as string}>
+      <Map
+        defaultCenter={{ lat, lng }}
+        defaultZoom={15}
+        mapId="2567b86b459988d06657407f"
+        className="w-full h-full"
+        disableDefaultUI={true}
+        
+      >
+        <Marker
+          position={{ lat, lng }}
+          icon={{
+            url: "/active-icon.svg", // always active icon
+          }}
+        />
+      </Map>
+      </APIProvider>
     </section>
   );
 };
 
-export default Map;
+export default MapOriginal;
